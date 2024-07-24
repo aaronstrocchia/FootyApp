2120eb714508612fdbb021196ade7353aaa6c6a0
Jose Ibarra <jibar030@fiu.edu>
2024-07-23 23:47:30 -0400

    added create team view, changed firebase

---
+++ Footy App/Footy App/CreateTeamView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTeamView.swift
+//  Footy App
+//
+//  Created by Jose Ibarra on 7/22/24.
+//
+
+import SwiftUI
+
+struct CreateTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTeamView()
+}
